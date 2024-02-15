4ed4fa646a71ce7de99d2340bec39cd0d1e13b4a
makoto-niko <mmana19970613@gmail.com>
2024-02-15 12:25:13 +0000

    25

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'home/about' =>'homes#about'
+  devise_for :users
+  resources :users, only: [:index, :show,:edit,:update] 
+  resources :books, only: [:show, :index, :edit, :create, :update, :destroy]
+end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
---
+++ db/migrate/20240215072812_add_user_id_tobook.rb
@@ -0,0 +1,4 @@
+class AddUserIdTobook < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
