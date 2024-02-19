9daf242d157dcd4d5fa985154924a61d2200431b
makoto-niko <mmana19970613@gmail.com>
2024-02-19 08:12:45 +0000

    33

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
   end
   
-  def index
-  end
-  
   def edit
   end
-end  
+   
+   private
+    def user_params
+      params.require(:user).permit(:name, :introduction, :image) 
+      end
+end
