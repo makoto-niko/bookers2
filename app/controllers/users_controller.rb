85dc38c4b364f7760f065a555e63c51bf76f4084
makoto-niko <mmana19970613@gmail.com>
2024-02-20 15:18:46 +0000

    43 add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images    
   end
   
   def edit
@@ -7,7 +14,7 @@ class UsersController < ApplicationController
   end
    
    private
-    def user_params
-      params.require(:user).permit(:name, :introduction, :image) 
+      def user_params
+       params.require(:user).permit(:name, :introduction, :image) 
       end
 end
