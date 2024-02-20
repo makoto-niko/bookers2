055188d63abff9f9581aad5015497d6f0d7ab259
makoto-niko <mmana19970613@gmail.com>
2024-02-20 13:33:47 +0000

    42

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,25 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
   
-  def destoroy 
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(list.id)  
+  end
+
+  def destroy 
      @book = Book.find(params[:id])
-     @book.destoroy
+     @book.destroy
      redirect_to books_path
   end
   
